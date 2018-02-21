6d8d22d0ee767e2e16da9073d560bacc12035361
do1youn <rhaxl89@gmail.com>
2018-02-21 17:14:23 +0900

    first

---
+++ project.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>dowon Youn - Portfolio</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+  <link rel="stylesheet" href="https://v40.pingendo.com/assets/bootstrap/bootstrap-4.0.0-beta.1.css" type="text/css">
+  <link rel="stylesheet" href="custom.css" type="text/css">
+  <style type="text/css">
+    .carousel-control-prev-icon,
+    .carousel-control-next-icon {
+      height: 100px;
+      width: 100px;
+      outline: none;
+      background-size: 100%, 100%;
+      border-radius: 50%;
+      border: 0px solid white;
+      background-image: none;
+    }
+
+    .carousel-control-next-icon:after {
+      content: '>';
+      font-size: 50px;
+      color: red;
+    }
+
+    .carousel-control-prev-icon:after {
+      content: '<';
+      font-size: 50px;
+      color: red;
+    }
+  </style>
+</head>
+
+<body>
+  <div class="py-5 text-center bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 p-4">
+          <div class="row">
+            <div class="col-md-12">
+              <a href="index.php">
+                <img class="img-fluid d-block mx-auto" src="../img/home.png" height="40" width="40"> </a>
+            </div>
+          </div>
+          <p class="m-3"><b>Contact</b>
+            <br>rhaxl89@gmail.com &amp; Use the chat.</p>
+          <div class="row">
+            <div class="col-md-3">
+              <a href="profile.html" class="btn btn-outline-dark">AboutMe</a>
+            </div>
+            <div class="col-md-3">
+              <div class="btn-group">
+                <button class="btn btn-dark dropdown-toggle " data-toggle="dropdown"><b><span style="font-weight: normal;"> Project#&nbsp;</span></b></button>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" href="project/project1.php">#1 학과커뮤니티Web</a>
+                  <a class="dropdown-item" href="project/project2.php">#2 건강검진알림Web App</a>
+                  <a class="dropdown-item" href="project/project3.php">#3 수면모니터링시스템</a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="project/thesis.php">#Thesis</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="btn-group">
+                <a href="experience.php" class="btn btn-outline-dark">Experience</a>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="btn-group">
+                <a href="self.php" class="btn btn-outline-dark">Self-development</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="bg-light py-3">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <p class="lead text-center text-capitalize"><b>Project</b></p>
+          <p class="mt-1 text-center">프로젝트 최신순으로 정렬하였습니다. 클릭하시면 자세한 정보를 볼 수 있습니다.
+            <br>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="py-5 bg-light"><div class="container"><div class="row"><div class="col-md-12"><div class="card">
+    <div class="card-header bg-dark text-light text-center"> Thesis#1: 뇌파측정용 해어밴드를 이용한 동작인식 알고리즘에 관한 연구 </div>
+    <div class="card-body text-center">
+      <img class="img-fluid mx-auto" src="img/features_mac.jpg" alt="Card image"><h5 contenteditable="true" class="my-3">2017.10 대한의료학회 학부생 논문</h5>
+
+      <p>Some quick example text to build on the card title .</p>
+    </div>
+  </div></div></div></div></div>
+
+  <div class="py-5 bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+        <a href="project/project3.php">  <img class="img-fluid d-block mx-auto rounded-circle" src="../img/240x240.png" width="250" height="250"> </a></div>
+        <div class="col-md-6">
+          <h5 class="m-1"><b>Project#3: Sleep Monitoring System</b></h5>
+          <p class="mt-1 m-2">&nbsp;학과 단일 커뮤니티 제작으로 학과생들이 중고 물품을 올리므로서 서로 친목을 도모하는 커뮤니티를 만들고자 함. 로그인이 반드시 필요하며 중고물품거래는 답글 형식으로 가능하도록 제작함.
+            <br>
+            <br> </p>
+          <h6 class="m-1"><b>나의 역활<br></b></h6>
+          <p class="mt-1 m-2"><b>자인에 따른 기능구현(자세히쓰기ㅣ긱기기)
+          </b></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-5 bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <a href="project/project2.php"><img class="img-fluid d-block mx-auto rounded-circle" src="../img/240x240.png" width="250" height="250"></a> </div>
+        <div class="col-md-6">
+          <h5 class="m-1"><b>Project#2: 건강검진알림서비스 WEB APP</b></h5>
+          <p class="mt-1 m-2">&nbsp;학과 단일 커뮤니티 제작으로 학과생들이 중고 물품을 올리므로서 서로 친목을 도모하는 커뮤니티를 만들고자 함. 로그인이 반드시 필요하며 중고물품거래는 답글 형식으로 가능하도록 제작함.
+            <br>
+            <br> </p>
+          <h6 class="m-1"><b>나의 역활<br></b></h6>
+          <p class="mt-1 m-2"><b>자인에 따른 기능구현(자세히쓰기ㅣ긱기기)
+          </b></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-5 bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+        <a href="project/project1.php">  <img class="img-fluid d-block mx-auto rounded-circle" src="../img/240x240.png" width="250" height="250"> </a></div>
+        <div class="col-md-6">
+          <h5 class="m-1"><b>Project#1: 학과 커뮤니티 WEB</b></h5>
+          <p class="mt-1 m-2">&nbsp;학과 단일 커뮤니티 제작으로 학과생들이 중고 물품을 올리므로서 서로 친목을 도모하는 커뮤니티를 만들고자 함. 로그인이 반드시 필요하며 중고물품거래는 답글 형식으로 가능하도록 제작함.
+            <br>
+            <br> </p>
+          <h6 class="m-1"><b>나의 역활<br></b></h6>
+          <p class="mt-1 m-2"><b>자인에 따른 기능구현(자세히쓰기ㅣ긱기기)
+          </b></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="text-center text-white bg-light p-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 p-5">
+          <a href="https://twitter.com/" class="text-secondary" target="_blank"><i class="fa fa-2x fa-git mx-1 text-dark" aria-hidden="true"></i></a>
+          <a href="https://plus.google.com/" class="text-danger" target="_blank"><i class="fa fa-fw fa-2x fa-google-plus mx-1 text-danger"></i></a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!--Start of Tawk.to Script-->
+  <script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/5a6818e1d7591465c707095a/default';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+  </script>
+  <!--End of Tawk.to Script-->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+  <pingendo onclick="window.open('https://pingendo.com/', '_blank')" style="cursor:pointer;position: fixed;bottom: 10px;right:10px;padding:4px;background-color: #00b0eb;border-radius: 8px; width:180px;display:flex;flex-direction:row;align-items:center;justify-content:center;font-size:14px;color:white">Made with Pingendo&nbsp;&nbsp;
+    <img src="https://pingendo.com/site-assets/Pingendo_logo_big.png" class="d-block" alt="Pingendo logo" height="16"> </pingendo>
+</body>
+
+</html>
