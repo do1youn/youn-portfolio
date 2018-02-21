596a97413755a94413f08ae50cbac744d89555d7
do1youn <rhaxl89@gmail.com>
2018-02-21 17:16:06 +0900

    first

---
+++ project/project1.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>dowon Youn - Portfolio</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+  <link rel="stylesheet" href="https://v40.pingendo.com/assets/bootstrap/bootstrap-4.0.0-beta.1.css" type="text/css">
+  <link rel="stylesheet" href="custom.css" type="text/css">
+  <style type="text/css">
+    .carousel-control-prev-icon,
+    .carousel-control-next-icon {
+      height: 100px;
+      width: 100px;
+      outline: none;
+      background-size: 100%, 100%;
+      border-radius: 50%;
+      border: 0px solid white;
+      background-image: none;
+    }
+
+    .carousel-control-next-icon:after {
+      content: '>';
+      font-size: 50px;
+      color: red;
+    }
+
+    .carousel-control-prev-icon:after {
+      content: '<';
+      font-size: 50px;
+      color: red;
+    }
+  </style>
+</head>
+
+<body>
+  <div class="py-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 p-4">
+          <div class="row">
+            <div class="col-md-12">
+              <a href="../index.php">
+                <img class="img-fluid d-block mx-auto" src="../img/home.png" height="40" width="40"> </a>
+            </div>
+          </div>
+          <p class="m-3 text-center"><b>Contact</b>
+            <br>rhaxl89@gmail.com &amp; Use the chat.</p>
+          <div class="row">
+            <div class="col-md-3">
+              <a href="../profile.html" class="btn btn-outline-dark">AboutMe</a>
+            </div>
+            <div class="col-md-3">
+              <div class="btn-group">
+                <button class="btn btn-dark dropdown-toggle " data-toggle="dropdown"><b><span style="font-weight: normal;"> Project#&nbsp;</span></b></button>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" href="project1.php">#1 학과장터Web site</a>
+                  <a class="dropdown-item" href="project2.php">#2 건강검진알림Web App</a>
+                  <a class="dropdown-item" href="project3.php">#3 수면모니터링시스템</a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="thesis.php">#Thesis</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="btn-group">
+                <a href="../experience.php" class="btn btn-outline-dark">Experience</a>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="btn-group">
+                <a href="../self.php" class="btn btn-outline-dark">Self-development</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-5 bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <img class="img-fluid d-block mx-auto rounded-circle" src="../img/240x240.png" width="250" height="250"> </div>
+        <div class="col-md-6">
+          <h5 class="m-1"><b>Project#1: 학과 장터 Web site</b></h5>
+          <p class="mt-1 m-2">&nbsp;학과 단일 커뮤니티 제작으로 학과생들이 중고 물품을 올리므로서 서로 친목을 도모하는 커뮤니티를 만들고자 함. 로그인이 반드시 필요하며 중고물품거래는 답글 형식으로 가능하도록 제작함.
+            <br>
+            <br> </p>
+          <h6 class="m-1"><b>나의 역활<br></b></h6>
+          <p class="mt-1 m-2"><b>자인에 따른 기능구현(자세히쓰기ㅣ긱기기)
+          </b></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-0">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <ul class="list-inline text-center m-4">
+            <ul class="list-inline text-center m-4">
+              <div class="row">
+                <div class="">
+                  <ul class="list-inline text-center m-4">
+                    <ul class="">
+                      <li class="text-left">참여 인원 : 3명</li>
+                      <li class="text-left">수행기간 : 2016.6 ~ 2017.12</li>
+                      <li class="text-left">개발도구 : 이클립스 0.0 / MYSQL0.0 /&nbsp;
+                        <br>
+                      </li>
+                    </ul>
+                  </ul>
+                </div>
+              </div>
+            </ul>
+          </ul>
+        </div>
+        <div class="col-md-6">
+          <ul class="list-inline text-center m-4">
+            <ul class="list-inline text-center m-4">
+              <div class="row">
+                <div class="">
+                  <ul class="list-inline text-center m-4">
+                    <ul class="">
+                      <li class="text-left">기술적특징1 : 게시글의 답글구현&nbsp;</li>
+                      <li class="text-left">기술적특징2 : JSP를 이용한 로그인/로그아웃처리&nbsp;</li>
+                      <li class="text-left">기술적특징3 : 최신글 목록리스트</li>
+                    </ul>
+                  </ul>
+                </div>
+              </div>
+            </ul>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-5 bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="row">
+            <div class=""> </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class=""> </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <h5 class="m-3"><b>Result</b></h5>
+        <div id="carousel" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item">
+              <img class="d-block img-fluid mx-auto" src="../img/600x450.png">
+              <div class="carousel-caption">
+                <h3>First slide label</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              </div>
+            </div>
+            <div class="carousel-item active">
+              <img class="d-block img-fluid mx-auto" src="../img/600x450.png">
+              <div class="carousel-caption">
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </div>
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev"> <span class="carousel-control-prev-icon" aria-hidden="true"></span> <span class="sr-only">Previous</span> </a>
+          <a class="carousel-control-next" href="#carousel" role="button" data-slide="next"> <span class="carousel-control-next-icon" aria-hidden="true"></span> <span class="sr-only">Next</span> </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="text-center text-white bg-light p-5">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 p-5">
+            <a href="https://twitter.com/" class="text-white" target="_blank"><i class="fa fa-2x fa-git mx-1 text-dark" aria-hidden="true"></i></a>
+            <a href="https://plus.google.com/" class="text-white" target="_blank"><i class="fa fa-fw fa-2x fa-google-plus mx-1 text-danger"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  <!--Start of Tawk.to Script-->
+
+  <script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/5a6818e1d7591465c707095a/default';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+  </script>
+  <!--End of Tawk.to Script-->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+  <pingendo onclick="window.open('https://pingendo.com/', '_blank')" style="cursor:pointer;position: fixed;bottom: 10px;right:10px;padding:4px;background-color: #00b0eb;border-radius: 8px; width:180px;display:flex;flex-direction:row;align-items:center;justify-content:center;font-size:14px;color:white">Made with Pingendo&nbsp;&nbsp;
+    <img src="https://pingendo.com/site-assets/Pingendo_logo_big.png" class="d-block" alt="Pingendo logo" height="16"> </pingendo>
+</body>
+
+</html>
