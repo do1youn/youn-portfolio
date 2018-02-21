3d59169e81cf1f2c2e0a1203d8a2c7be73ce6523
do1youn <rhaxl89@gmail.com>
2018-02-21 17:13:34 +0900

    second

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>dowon Youn - Portfolio</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+  <link rel="stylesheet" href="https://v40.pingendo.com/assets/bootstrap/bootstrap-4.0.0-beta.1.css" type="text/css">
+  <link rel="stylesheet" href="custom.css" type="text/css"> </head>
+
+<body>
+  <div class="py-5 text-center bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1>dowon youn - Portfolio</h1>
+          <p class="lead">Welcome My web site!
+            <br>Take your time and look around.</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12 p-4">
+          <img class="img-fluid d-block mx-auto rounded-circle" src="img/frofile2.jpg" width="250" height="250">
+          <p class="my-4"><i>안녕하세요. 저의 Portflio site에 방문해주셔서 진심으로 고맙습니다!<br>
+                  <br>이렇게 제 사이트를 방문해 주신 건 저에게 또는 제 작업에 관심이 있으셨기 때문이라고 생각합니다.
+                  <br>천천히 둘러보시고 부족한 점이나 아쉬운 점, 좋았던 점이 있다면 메일을 통해 알려주세요!
+                  <br>더 좋은 사이트, 더 나은 개발자가 되는 좋은 피드백이 될 것 같습니다 :)
+                  <br></i></p>
+          <div class="row">
+            <div class="col-md-12">
+              <a href="index.html">
+                <img class="img-fluid d-block mx-auto" src="img/home.png" height="40" width="40"> </a>
+            </div>
+          </div>
+          <p><b>Contact</b>
+            <br>rhaxl89@gmail.com &amp; Use the chat.</p>
+          <div class="row">
+            <div class="col-md-3">
+              <a href="index.php" class="btn btn-outline-dark">AboutMe</a>
+            </div>
+            <div class="col-md-3">
+              <a href="project.php" class="btn btn-outline-dark">Project#&nbsp;</a>
+            </div>
+            <div class="col-md-3">
+              <a href="experience.php" class="btn btn-outline-dark">Experience</a>
+            </div>
+            <div class="col-md-3">
+              <a href="self.php" class="btn btn-outline-dark">Self-development</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--?php
+echo file_get_contents("project/".$_GET['id']);
+ ?-->
+  <div class="py-5 text-center bg-light">
+    <div class="container">
+      <p> </p>
+      <div id="disqus_thread"></div>
+      <script>
+        /**
+        *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+        *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+        /*
+        var disqus_config = function () {
+        this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+        this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+        };
+        */
+        (function() { // DON'T EDIT BELOW THIS LINE
+        var d = document, s = d.createElement('script');
+        s.src = 'https://dowon.disqus.com/embed.js';
+        s.setAttribute('data-timestamp', +new Date());
+        (d.head || d.body).appendChild(s);
+        })();
+      </script> <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+      <p></p>
+    </div>
+  </div>
+  <!--Start of Tawk.to Script-->
+  <script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/5a6818e1d7591465c707095a/default';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+  </script>
+  <!--End of Tawk.to Script-->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" crossorigin="anonymous" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+</body>
+
+</html>
